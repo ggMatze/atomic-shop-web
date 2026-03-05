055a1a7433a86ce26f03761032bd5d78ca7f5b09
ggMatze <29570333+ggMatze@users.noreply.github.com>
2026-03-05 21:35:34 +0100

    Update

--- src/ui.js
+++ src/ui.js
@@ -36,6 +36,9 @@ function attachTileClickHandlers() {
     html = html.replace(/\[([a-zA-Z][\w-]*)\]([\s\S]*?)\[\/\1\]/g, function(_, cls, inner) {
       return `<span class="${cls}">${inner}</span>`;
     });
+    html = html.replace(/\[url=([^\]]+)\]([\s\S]*?)\[\/url\]/g, function(_, url, text) {
+      return `<a href="${url}" target="_blank" rel="noopener noreferrer" class="overlay-link">${text || url} <svg width="18" height="18" viewBox="2 3 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: middle; margin-left: 2px;"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/><polyline points="15,3 21,3 21,9"/><line x1="10" y1="14" x2="21" y2="3"/></svg></a>`;
+    });
     return html;
   };
 
